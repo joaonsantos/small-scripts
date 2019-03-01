3c0919939edd99c6d48829a08dddee1a81996ca3
João Santos <joaopns05@gmail.com>
2019-03-01 14:56:14 +0000

    Apply new formatting.

--- install-r.sh
+++ install-r.sh
@@ -1,41 +1,41 @@
 get_distribution() {
-	lsb_dist=""
+  lsb_dist=""
 
-	if [ -r /etc/os-release ]; then
-		lsb_dist="$(. /etc/os-release && echo "$ID")"
-	fi
-	# Returning an empty string here should be alright since the
-	# case statements don't act unless you provide an actual value
-	echo "$lsb_dist"
+  if [ -r /etc/os-release ]; then
+    lsb_dist="$(. /etc/os-release && echo "$ID")"
+  fi
+  # Returning an empty string here should be alright since the
+  # case statements don't act unless you provide an actual value
+  echo "$lsb_dist"
 }
 
 get_distribution_version() {
-# perform some very rudimentary platform detection
-	lsb_dist=$( get_distribution )
-	lsb_dist="$(echo "$lsb_dist" | tr '[:upper:]' '[:lower:]')"
+  # perform some very rudimentary platform detection
+  lsb_dist=$( get_distribution )
+  lsb_dist="$(echo "$lsb_dist" | tr '[:upper:]' '[:lower:]')"
 
-	case "$lsb_dist" in
+  case "$lsb_dist" in
 
-		ubuntu|linuxmint)
+    ubuntu|linuxmint)
       dist_version="$(lsb_release --codename | cut -f2)"
 
-			if [ -z "$dist_version" ] && [ -r /etc/lsb-release ]; then
-				dist_version="$(. /etc/lsb-release && echo "$DISTRIB_CODENAME")"
-			fi
-		;;
+      if [ -z "$dist_version" ] && [ -r /etc/lsb-release ]; then
+        dist_version="$(. /etc/lsb-release && echo "$DISTRIB_CODENAME")"
+      fi
+      ;;
 
-	esac
+    esac
 
-  echo "$dist_version" 
-}
+    echo "$dist_version" 
+  }
 
 do_install() {
   echo -e 'Starting installation...\n'
   dist_version=$( get_distribution_version )
 
-	case "$dist_version" in
+  case "$dist_version" in
 
-		tessa|bionic)
+    tessa|bionic)
       echo 'deb https://cloud.r-project.org/bin/linux/ubuntu cosmic-cran35/' >> \
         sudo tee -a /etc/apt/sources.list
 
@@ -45,13 +45,13 @@ do_install() {
 
       sudo apt-get update
       sudo apt-get install -y r-base
-		;;
+      ;;
 
-	esac
+    esac
 
-  echo -e 'Installation finished successfully.'
-  echo -e 'Use R command or Rscripts command to start your journey in r!\n'
-}
+    echo -e 'Installation finished successfully.'
+    echo -e 'Use R command or Rscripts command to start your journey in r!\n'
+  }
 
 # Run the script
 do_install
