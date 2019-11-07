30e99597a96fe8a92e2df85b52288343e6b58de5
João Santos <joaopns05@gmail.com>
2019-11-07 11:40:52 +0000

    Add new file

---
+++ disable-vim-arrow-keys.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo 'nnoremap <left> <nop>' >> ~/.vimrc
+echo 'nnoremap <right> <nop>' >> ~/.vimrc
+echo 'nnoremap <up> <nop>' >> ~/.vimrc
+echo 'nnoremap <down> <nop>' >> ~/.vimrc
+
+echo -e 'Completed successfully.\n'
