df7623956c8cb363e4eaa8c9b4295626a6f70a58
João Santos <joaopns05@gmail.com>
2020-02-17 11:57:29 +0000

    Update to reflect docker package for eoan

--- install-docker.sh
+++ install-docker.sh
@@ -39,20 +39,11 @@ do_install() {
 
   case "$dist_version" in
 
-    disco)
+    disco | eoan)
       echo -e "Using Ubuntu ${dist_version}, proceeding with install...\n"
       curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
       sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable edge"
-      sudo apt-get install -y docker-ce
-
-      ;;
-    eoan)
-      echo -e "Using Ubuntu ${dist_version}, proceeding with install...\n"
-      curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-      sudo add-apt-repository "deb [arch=amd64]
-      https://download.docker.com/linux/ubuntu disco stable edge"
       sudo apt-get install -y docker-ce
 
       ;;
