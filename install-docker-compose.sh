bab0b05afe3c6b784e8d62cbe845c1f17cb34120
João Santos <joaopns05@gmail.com>
2020-02-17 12:00:11 +0000

    Bump docker compose version

--- install-docker-compose.sh
+++ install-docker-compose.sh
@@ -2,7 +2,7 @@
 # Exit on first error, print all commands
 set -ev
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
 
