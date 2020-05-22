d26a3931c22b3ec368b53aa7afc94cc73680dd90
João Santos <joaopns05@gmail.com>
2020-05-22 11:15:53 +0100

    Remove unmaintained scripts

--- disable-vim-arrow-keys.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo 'nnoremap <left> <nop>' >> ~/.vimrc
-echo 'nnoremap <right> <nop>' >> ~/.vimrc
-echo 'nnoremap <up> <nop>' >> ~/.vimrc
-echo 'nnoremap <down> <nop>' >> ~/.vimrc
-
-echo -e 'Completed successfully.\n'
