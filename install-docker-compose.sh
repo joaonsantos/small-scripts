3894577a34cc844a3e0e7373fcf7feefe2872cad
João Santos <joaopns05@gmail.com>
2021-08-20 18:36:00 +0100

    remove unmaintained scripts

--- install-docker-compose.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Exit on first error, print all commands
-set -ev
-
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-sudo chmod +x /usr/local/bin/docker-compose
-
-echo "Docker-compose installed successfully!"
